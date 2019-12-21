299a9570278e8be5a93194c40d61b7f630787e87
Sebas <seplarui@gmail.com>
2019-12-21 22:27:02 +0100

    Move files

---
+++ src/Exer_6_.java
@@ -0,0 +1,13 @@
+public class Exer_6 {
+
+     void whoLikesIt();
+
+    class Solution {
+        public void whoLikesIt(String... names) {
+            for (String i : names) {
+                System.out.println("I-->" + i);
+            }
+        }
+    }
+
+}
